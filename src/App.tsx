7a513c3b3c4cc6dc8faa0b1da0fcad920a385856
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-01-22 02:22:54 -0300

    chore: install firebase and your config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-function App() {
+const App = () => {
   return (
     <div>
-      <h1>Ola mundo</h1>
+      <h1 className="">Ola mundo</h1>
     </div>
   )
 }
---
+++ src/lib/firebase.config.ts
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDM1cDF19BYLpibBv5f-WMhNYmMwAUogyA',
+  authDomain: 'ecommerce-x-23610.firebaseapp.com',
+  projectId: 'ecommerce-x-23610',
+  storageBucket: 'ecommerce-x-23610.firebasestorage.app',
+  messagingSenderId: '551343058482',
+  appId: '1:551343058482:web:c07b32c9bee976e562962b',
+  measurementId: 'G-0ZH8NES18B',
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
