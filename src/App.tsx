47096774a33476bcbbcb69224497855d1dc22484
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-01-23 02:15:18 -0300

    chore: add firestore config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from './lib/firestore.config'
+import { useEffect } from 'react'
+
 const App = () => {
+  useEffect(() => {
+    const getUsers = async () => {
+      const querySnapshot = await getDocs(collection(db, 'users'))
+      const user = querySnapshot.docs.map((doc) => doc.data())
+      console.log(user)
+    }
+    getUsers()
+  }, [])
+
   return (
     <div>
-      <h1 className="">Ola mundo</h1>
+      <h1 className=""></h1>
     </div>
   )
 }
