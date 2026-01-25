9840029026e12e943178802cd7b1dc2540b50b11
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-01-25 20:08:31 -0300

    refactor: name component changed

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,15 @@
 import Categories from '../components/categories'
 
-const Home = () => {
+const HomePage = () => {
   return (
     <div>
       <Categories />
+      <img
+        src="https://unsplash.com/pt-br/fotografias/homem-de-topless-em-shorts-pretos-sentados-em-barra-preta-e-prateada-9dzWZQWZMdE"
+        alt="img"
+      />
     </div>
   )
 }
 
-export default Home
+export default HomePage
