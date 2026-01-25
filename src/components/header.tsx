b022eeb590917d51db9ab12d9e6c564b2d307602
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-01-25 04:35:50 -0300

    feat: create header component

---
+++ src/components/header.tsx
@@ -0,0 +1,12 @@
+import LayoutContent from '../layout/layout-content'
+
+const Header = () => {
+  return (
+    <LayoutContent className="flex h-16 w-full items-center justify-between bg-purple-950">
+      <img src="./src/assets/logo.png" alt="logo" />
+      <h1>asdj</h1>
+    </LayoutContent>
+  )
+}
+
+export default Header
