bef1671531ed546e55f8e2b3a05d18d227e8dc6b
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-01-29 02:28:16 -0300

    refactor: add app with params to getAuth

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -15,4 +15,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
-export const auth = getAuth()
+export const auth = getAuth(app)
