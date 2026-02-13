1abf8dcc9aee84ec03b83f91070591d160cb5579
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-02-13 18:03:49 -0300

    feat: add hero photo

---
+++ src/components/home/hero.tsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+  return (
+    <div className="absolute inset-0 z-0">
+      <img
+        src="./src/assets/hero.png"
+        alt="Hero"
+        className="mask h-full w-full mask-b-from-85% object-cover object-center"
+      />
+    </div>
+  )
+}
+
+export default Hero
