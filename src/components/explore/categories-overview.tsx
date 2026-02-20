ae2f0e2a0619a33609ef0a04c6ab31585727d06b
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-02-20 00:36:14 -0300

    feat: add categories-overview component

---
+++ src/components/explore/categories-overview.tsx
@@ -0,0 +1,28 @@
+import { LayoutContent } from '../../layout/layout'
+import ArrowDown from '../../assets/arrow-down.png'
+import { useContext, useEffect } from 'react'
+import { CategoriesContext } from '../../contexts/category-context'
+import CategoryOverview from './category-overview'
+
+const CategoriesOverview = () => {
+  const { categories, fetchCategories } = useContext(CategoriesContext)
+
+  useEffect(() => {
+    fetchCategories()
+  }, [fetchCategories])
+  return (
+    <LayoutContent className="mt-18 flex w-full flex-col items-center space-y-10">
+      <div className="flex flex-col items-center gap-8">
+        <h1 className="font-pump text-3xl font-semibold">
+          Explorar os mais vendidos
+        </h1>
+        <img src={ArrowDown} alt="arrow-down" className="animate-bounce" />
+      </div>
+      <div className="flex w-full flex-wrap gap-12">
+        <CategoryOverview categories={categories} />
+      </div>
+    </LayoutContent>
+  )
+}
+
+export default CategoriesOverview
