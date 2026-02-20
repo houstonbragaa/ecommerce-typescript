120dd8631393d7606f297a7478e8c6a2e7c2285f
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-02-20 20:28:56 -0300

    feat: add items-center

--- src/components/explore/category-overview.tsx
+++ src/components/explore/category-overview.tsx
@@ -7,9 +7,9 @@ interface ICategoryOverview {
 
 const CategoryOverview = ({ categories }: ICategoryOverview) => {
   return (
-    <div className="flex w-full flex-col gap-10">
+    <div className="flex w-full flex-col items-center gap-10">
       {categories?.map((category) => (
-        <section key={category.id} className="flex flex-col gap-4">
+        <section key={category.id} className="flex flex-col items-start gap-4">
           <h2 className="text-xl font-semibold">{category.displayName}</h2>
           <div className="flex gap-4 overflow-x-auto">
             {category.products.slice(0, 4).map((product) => (
