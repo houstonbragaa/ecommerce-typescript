12dec29a2baf0f073ad154ee942d5f875cade46c
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-02-24 20:59:38 -0300

    fix: fix the problem to redirect category page

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,5 +1,6 @@
 import { Search, ShoppingCart } from 'lucide-react'
 import { LayoutHeader } from '../../layout/layout'
+import logoImg from '../../assets/logo.png'
 import { useContext, useState } from 'react'
 import { signOut } from 'firebase/auth'
 import { auth } from '../../config/firebase'
@@ -24,11 +25,7 @@ const Header = () => {
     <header className="sticky top-0 right-0 left-0 z-50 flex justify-center px-2 py-5">
       <LayoutHeader className="flex h-16 w-full max-w-3xl items-center justify-between rounded-full border border-white/20 shadow-xl shadow-purple-700/20 backdrop-blur-md sm:w-3xl">
         <a href="/">
-          <img
-            src="./src/assets/logo.png"
-            alt="logo"
-            className="ml-2 h-10 w-10"
-          />
+          <img src={logoImg} alt="logo" className="ml-2 h-10 w-10" />
         </a>
 
         <div className="flex items-center gap-2">
