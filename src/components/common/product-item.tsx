f05dbd600588083ba89f2c7574a8bb5e491661c3
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-03-08 12:00:21 -0300

    refactor: simplify product item button click handler and adjust image mask

--- src/components/common/product-item.tsx
+++ src/components/common/product-item.tsx
@@ -10,23 +10,19 @@ interface IProductItemProps {
 const ProductItem = ({ product }: IProductItemProps) => {
   const { addProductToCart } = useContext(CartContext)
 
-  const handleAddProductToCartClick = () => {
-    addProductToCart(product)
-  }
-
   return (
     <div className="flex flex-col items-start gap-5">
       <div className="relative h-[200px] w-[250px] overflow-hidden rounded-xs">
         <img
           src={product.imageUrl}
           alt={product.name}
-          className="absolute inset-0 h-full w-full mask-b-from-15% object-cover"
+          className="absolute inset-0 h-full w-full mask-b-from-5% object-cover"
         />
       </div>
       <p className="font-medium text-white drop-shadow-lg">{product.name}</p>
       <span className="text-md font-semibold text-zinc-400">{`R$ ${product.price},00`}</span>
       <button
-        onClick={handleAddProductToCartClick}
+        onClick={() => addProductToCart(product)}
         className="flex cursor-pointer items-center justify-center gap-2 rounded-md border border-white/20 bg-transparent px-2 py-1 text-white/80 hover:bg-white/20"
       >
         Adicionar ao carrinho
