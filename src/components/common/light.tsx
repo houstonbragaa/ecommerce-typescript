8237b2e488d418911c5870572e4c48eadacb08ff
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-03-09 01:08:32 -0300

    feat: implement dynamic purple light orbs on homepage for enhanced visual appeal

---
+++ src/components/common/light.tsx
@@ -0,0 +1,60 @@
+interface LightConfig {
+  top: string
+  className: string
+}
+
+const PURPLE_LIGHTS: LightConfig[] = [
+  {
+    top: '15vh',
+    className: '-left-40 h-[350px] w-[350px] bg-violet-600/20',
+  },
+  {
+    top: '35vh',
+    className: '-right-40 h-[300px] w-[300px] bg-purple-600/15',
+  },
+  {
+    top: '55vh',
+    className: 'left-1/2 h-[280px] w-[450px] -translate-x-1/2 bg-fuchsia-500/7',
+  },
+  {
+    top: '75vh',
+    className: 'right-[5%] h-[220px] w-[220px] bg-violet-500/13',
+  },
+  {
+    top: '65vh',
+    className: 'left-[8%] h-[180px] w-[180px] bg-purple-400/10',
+  },
+  {
+    top: '120vh',
+    className: '-left-32 h-[250px] w-[250px] bg-violet-700/15',
+  },
+  {
+    top: '180vh',
+    className: '-right-32 h-[200px] w-[200px] bg-purple-500/10',
+  },
+  {
+    top: '240vh',
+    className: 'left-[20%] h-[220px] w-[220px] bg-violet-400/7',
+  },
+  {
+    top: '300vh',
+    className: 'right-[25%] h-[190px] w-[190px] bg-purple-600/10',
+  },
+]
+
+const PurpleLights = () => {
+  return (
+    <>
+      {PURPLE_LIGHTS.map((light, index) => (
+        <div
+          key={index}
+          className={`purple-light-orb fixed ${light.className}`}
+          style={{ top: light.top }}
+          aria-hidden
+        />
+      ))}
+    </>
+  )
+}
+
+export default PurpleLights
--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,5 +1,6 @@
 import Footer from '../components/common/footer'
 import Header from '../components/common/header'
+import PurpleLights from '../components/common/light'
 import About from '../components/home/about'
 import CategoriesSection from '../components/home/categories-section'
 import FeaturedByCategory from '../components/home/featured-by-category'
@@ -10,47 +11,9 @@ import { LayoutContainer } from '../layout/layout'
 
 const HomePage = () => {
   return (
-    <LayoutContainer className="relative flex w-full flex-col overflow-x-hidden md:items-center">
-      {/* Luzes estáticas roxas - vários pontos da página */}
-      <div
-        className="purple-light-orb absolute top-[15%] -left-40 h-[350px] w-[350px] bg-violet-600/25"
-        aria-hidden
-      />
-      <div
-        className="purple-light-orb absolute top-[35%] -right-40 h-[300px] w-[300px] bg-purple-600/20"
-        aria-hidden
-      />
-      <div
-        className="purple-light-orb absolute top-[55%] left-1/2 h-[280px] w-[450px] -translate-x-1/2 bg-fuchsia-500/12"
-        aria-hidden
-      />
-      <div
-        className="purple-light-orb absolute top-[75%] right-[5%] h-[220px] w-[220px] bg-violet-500/18"
-        aria-hidden
-      />
-      <div
-        className="purple-light-orb absolute top-[65%] left-[8%] h-[180px] w-[180px] bg-purple-400/15"
-        aria-hidden
-      />
-      <div
-        className="purple-light-orb absolute top-[95%] -left-32 h-[250px] w-[250px] bg-violet-700/20"
-        aria-hidden
-      />
-      <div
-        className="purple-light-orb absolute top-[115%] -right-32 h-[200px] w-[200px] bg-purple-500/15"
-        aria-hidden
-      />
-      <div
-        className="purple-light-orb absolute top-[140%] left-[20%] h-[220px] w-[220px] bg-violet-400/12"
-        aria-hidden
-      />
-      <div
-        className="purple-light-orb absolute top-[165%] right-[25%] h-[190px] w-[190px] bg-purple-600/15"
-        aria-hidden
-      />
-
+    <LayoutContainer className="relative flex w-full flex-col md:items-center">
+      <PurpleLights />
       <Header />
-
       {/* Margem negativa puxa a hero para cima, ficando atrás do header sticky */}
       <div className="relative -mt-[100px] min-h-screen w-full">
         <Hero />
