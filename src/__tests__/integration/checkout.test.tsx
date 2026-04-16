1c55bdce2e1fd40ffa55cf86002e410cb9cde65c
houstonbragaa <houstonbragadeveloper@gmail.com>
2026-04-16 00:54:12 -0300

    test: add checkout tests

---
+++ src/__tests__/integration/checkout.test.tsx
@@ -0,0 +1,60 @@
+import { render, screen } from '@testing-library/react'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+
+import Checkout from '../../components/checkout/checkout'
+import { selectTotalPrice, useCartStore } from '../../stores/cart-store'
+
+vi.mock('../../stores/cart-store', () => ({
+  useCartStore: vi.fn(),
+  selectTotalPrice: vi.fn(),
+}))
+
+const mockedCartStore = vi.mocked(useCartStore)
+
+const mockProducts = [
+  {
+    id: '1',
+    name: 'Produto A',
+    price: 100,
+    imageUrl: 'image_url',
+    quantity: 1,
+  },
+  {
+    id: '2',
+    name: 'Produto B',
+    price: 200,
+    imageUrl: 'image_url',
+    quantity: 2,
+  },
+]
+
+describe('checkout', () => {
+  beforeEach(() => {
+    vi.clearAllMocks()
+  })
+
+  it('should render ckeckout component correctly with products', () => {
+    mockedCartStore.mockImplementation((selector) => {
+      if (selector === selectTotalPrice) return 500
+      return mockProducts
+    })
+
+    render(<Checkout />)
+
+    expect(screen.getByText(/checkout/i)).toBeInTheDocument()
+    expect(screen.getByTestId('cart-total-price')).toHaveTextContent(
+      'R$ 500.00'
+    )
+  })
+
+  it('should message checkout empty', () => {
+    mockedCartStore.mockImplementation((selector) => {
+      if (selector === selectTotalPrice) return 0
+      return []
+    })
+
+    render(<Checkout />)
+
+    expect(screen.getByText('O carrinho está vazio!')).toBeInTheDocument()
+  })
+})
--- src/components/checkout/checkout.tsx
+++ src/components/checkout/checkout.tsx
@@ -25,12 +25,17 @@ const Checkout = () => {
     <LayoutContent className="-mt-16 flex h-screen w-full flex-col items-center justify-center space-y-4">
       <h1 className="font-pump text-center text-3xl font-semibold">Checkout</h1>
       <div className="scrollbar-thin-light flex h-[400px] w-full flex-col gap-4 overflow-y-scroll px-4 py-4 sm:w-[500px]">
+        {products.length == 0 && <p>O carrinho está vazio!</p>}
+
         {products.map((product) => (
           <CartItem key={product.id} product={product} />
         ))}
       </div>
       <div className="flex w-full flex-col justify-start gap-2 sm:w-[500px]">
-        <p className="font-bold">{`R$ ${totalPrice},00`}</p>
+        <p
+          data-testid="cart-total-price"
+          className="font-bold"
+        >{`R$ ${totalPrice.toFixed(2)}`}</p>
         <button
           onClick={handleFinishOrder}
           className="flex w-full cursor-pointer items-center justify-center gap-2 rounded-lg bg-purple-900 py-1 hover:bg-purple-900/80"
